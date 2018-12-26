2685a84824e9599ec7e915dd09bdea5f7280f42a
Catalin Mustata <catalin.mustata@gmail.com>
2018-12-26 12:34:47 +0200

    Initial Commit

---
+++ Warehouse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Warehouse
+//
+//  Created by Catalin Mustata on 26/12/2018.
+//  Copyright © 2018 BearSoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
